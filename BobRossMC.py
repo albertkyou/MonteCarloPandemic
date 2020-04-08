b7661f1b93f719ec6b33142d54b09e49a64b5990
albertkyou <albert.you@berkeley.edu>
2020-04-08 13:59:37 -0700

    Random Walk
    BobRossMC is a happy little mistake that makes very pretty plots.

--- BobRossMC.py
+++ BobRossMC.py
@@ -7,26 +7,20 @@ import matplotlib.pyplot as plt
 
 def initialize_board(num_people):
     # calculate initial positions of people
-    init_pos = 500*np.random.rand(num_people,2) # x,y positions of num_people people in 500x500 grid
+    init_pos = 100*np.random.rand(num_people,2) # x,y positions of num_people people in 100x100 grid
 
     return init_pos
 
 def random_walk_step(init_pos, speed):
     num_people = init_pos.shape[0]
     pos = init_pos + speed*(2*np.random.rand(num_people,2)-1)
-    # make bounds
-    for i in range(pos.shape[0]):
-        if pos[i,0] < 0 or pos[i,0] > 500:
-            pos[i,0] = init_pos[i,0]
-        if pos[i,1] < 0 or pos[i,1] > 500:
-            pos[i,1] = init_pos[i,1]
 
     return pos
 
 def draw_dots(pos,screen):
 
     for i in range(pos.shape[0]):
-        pygame.draw.circle(screen,(0,0,0),(int(pos[i,0]),int(pos[i,1])),2)
+        pygame.draw.circle(screen,(0,0,0),(int(pos[i,0]),int(pos[i,1])),1)
     
     pygame.display.update()
 
@@ -37,10 +31,10 @@ def draw_dots(pos,screen):
 
 # Display the counter for dots inside the circle and outside the circle
 
-pygame.init()
+
 
 def main():
-
+    pygame.init()
 
     background_colour = (255,255,255) # white
     (width, height) = (500,500)
@@ -49,7 +43,7 @@ def main():
     pygame.display.set_caption('Approximate Pi')
     screen.fill(background_colour)
 
-    pos = initialize_board(100)
+    pos = initialize_board(1)
     running = True
     while running:
         for event in pygame.event.get():
@@ -57,8 +51,8 @@ def main():
                 running = False
 
         pos = random_walk_step(pos,5)
-        screen.fill((255,255,255))
+
         draw_dots(pos,screen)
-        pygame.time.wait(50)
+
 
 main()
