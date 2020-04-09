6eb3c756c59b11260806e8ac6747b1e6d643ec1f
albertkyou <albert.you@berkeley.edu>
2020-04-08 17:23:11 -0700

    Update MCSim.py

--- MCSim.py
+++ MCSim.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 # Create a Monte Carlo simulation of random walk.
 
-def initialize_board(num_people):
+def initialize_board(num_people=100):
     # calculate initial positions of people
     init_pos = 500*np.random.rand(num_people,2) # x,y positions of num_people people in 500x500 grid
     infected = [np.random.randint(num_people+1)] # list of infected individuals
 
     return init_pos, infected
 
-def random_walk_step(init_pos, speed,infected):
+def random_walk_step(init_pos, speed=20 ,infected=1):
     num_people = init_pos.shape[0]
     pos = init_pos + speed*(2*np.random.rand(num_people,2)-1)
     # make bounds
@@ -26,16 +26,15 @@ def random_walk_step(init_pos, speed,infected):
 
     return pos
 
-def near_infected(infected,pos,person):
+def near_infected(infected,pos,person, social_distance=6, r0=2):
     # calculate distance between person and all infected individuals
-    social_distance = 6 
-
     for individual in infected:
         infected_individual_position = pos[individual,:]
 
-        distance = np.sqrt((person[0]-infected_individual_position[0])**2+(person[0]-infected_individual_position[0])**2)
+        distance = np.sqrt((person[0]-infected_individual_position[0])**2+(person[1]-infected_individual_position[1])**2)
         if distance < social_distance:
-            return True
+            if r0*np.random.rand()>1:
+                return True
     
     return False
 
@@ -44,21 +43,18 @@ def draw_dots(pos,infected,screen):
     for i in range(pos.shape[0]):
         if i in infected:
             color = (255,0,0)
+            marker_size = 4
         else:
             color = (0,0,0)
+            marker_size = 2
         
-        pygame.draw.circle(screen,color,(int(pos[i,0]),int(pos[i,1])),2)
+        pygame.draw.circle(screen,color,(int(pos[i,0]),int(pos[i,1])),marker_size)
     
     pygame.display.update()
 
 
 
-# Now we need to wrap those functions in a pygame window
-
-# Approximate social distancing by saying the chance of infection as dependent on the distance to those infected
-
-# Display the counter for dots inside the circle and outside the circle
-
+##### Now we need to wrap those functions in a pygame window
 pygame.init()
 
 def main():
@@ -71,14 +67,14 @@ def main():
     pygame.display.set_caption('Approximate Pi')
     screen.fill(background_colour)
 
-    pos, infected = initialize_board(300)
+    pos, infected = initialize_board()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        pos = random_walk_step(pos,5,infected)
+        pos = random_walk_step(pos,10,infected)
         screen.fill((255,255,255))
         draw_dots(pos,infected,screen)
         pygame.time.wait(50)
